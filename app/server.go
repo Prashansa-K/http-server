a33a9694fb1d29759bc388d3910ff0a7c26ec1f2
Prashansa Kulshrestha <prashkulshrestha@gmail.com>
2024-06-26 15:09:52 +0530

    Added 200 ok response

--- app/server.go
+++ app/server.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+const (
+	CRLF = "\r\n"
+)
+
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
@@ -16,9 +20,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+
+	okResponse := fmt.Sprintf("HTTP/1.1 200 OK%s%s", CRLF, CRLF)
+
+	_, err = conn.Write([]byte(okResponse))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
 		os.Exit(1)
 	}
 }
