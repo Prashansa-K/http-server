862043cc649c34a3a9d4f716d645c9af7957ddfb
Prashansa Kulshrestha <prashkulshrestha@gmail.com>
2024-06-28 12:44:37 +0530

    Testing echo endpoint

--- app/serverRoutes.go
+++ app/serverRoutes.go
@@ -10,18 +10,19 @@ func serveEcho(w http.ResponseWriter, r *http.Request) {
 	path := r.URL.Path
 
 	s := strings.Split(path, "/")
-	fmt.Println(s)
+	// fmt.Println(s)
 
 	if len(s) > 1 && s[2] != "" {
-		bodySize := len(s[2])
-		contentType := "Content-Type: text/plain\r\n"
-		contentLen := fmt.Sprintf("Content-Length: %d%s", bodySize, CRLF)
-		w.Write([]byte(
-			OK_RESPONSE +
-				contentType +
-				contentLen +
-				s[2],
-		))
+		bodySize := fmt.Sprintf("%d", len(s[2]))
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Header().Set("Content-Length", bodySize)
+
+		// headers should be set before WriteHeader and Write functions (unless we are sending 1xx codes)
+		// otherwise default ones would be sent
+		// trailers can be added afterwards too
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(s[2]))
 	}
 
 	// w.Write([]byte(OK_RESPONSE))
